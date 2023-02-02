979abb08b84626b1de244fe39d5c84cc5a846160
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-03 00:54:14 +0900

    Initial Commit

---
+++ FruitListApp/FruitListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitListAppApp.swift
+//  FruitListApp
+//
+//  Created by 渡邊魁優 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct FruitListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
